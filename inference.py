0cae525e81b59eb793e569772dd2389e77ee4f38
SilverSushi <bluejaywhale@gmail.com>
2026-08-23 16:26:07 +0900

    em data

--- inference.py
+++ inference.py
@@ -175,7 +175,7 @@ def evaluate(
 
 if __name__ == "__main__":
     evaluate(
-        ckpt_path="checkpoints/ckpt_best.pt",
+        ckpt_path="checkpoints/em_ckpt_best_v1.pt",
         n_steps=1,
         batch_size=128,
         device="cuda",
